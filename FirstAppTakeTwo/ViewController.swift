1b35a75b7358f47bfd4e649d1b4827d0de09f7e7
Omar <norombabaor@appstate.edu>
2022-08-25 13:12:07 -0700

    Initial Commit

---
+++ FirstAppTakeTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstAppTakeTwo
+//
+//  Created by user226354 on 8/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
